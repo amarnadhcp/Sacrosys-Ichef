db948023ff3483c4378cb9d09140209441cdab5b
Amarnadh <cpamarnadh843@gmail.com>
2024-03-05 14:29:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css'
+import Layout from './pages/Layout';
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+       <Routes>
+         <Route path="/" element={<Layout/>} />
+
+         
+       </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
