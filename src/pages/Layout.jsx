435dec1b282b1e1f9c435b547be753f8cc5a8a5b
Amarnadh <cpamarnadh843@gmail.com>
2024-03-06 11:31:43 +0530

    feat(navbar-deisgn ongoing)

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import count from "../assets/count.svg";
 
 function Layout() {
   return (
     <div className="h-screen w-full flex flex-col">
       <div className="flex-grow bg-gradient-to-br from-[#FF4F5F] to-[#241046] flex flex-col">
         <Navbar/>
+        
       </div>
     </div>
   );
